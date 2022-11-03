c98a87529076ba9f3743492b653ac98e6b713ebc
0wissem <wissem.ksentini@gmail.com>
2022-11-03 18:39:24 +0100

    testing add input component

---
+++ src/components/AddInput/AddInput.test.js
@@ -0,0 +1,35 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import AddInput from "./AddInput";
+const mockedSetTodos = jest.fn();
+describe("AddInput", () => {
+  it("Should render input component", () => {
+    render(<AddInput todos={[]} setTodos={mockedSetTodos} />);
+    const inputComponent = screen.getByPlaceholderText(
+      "Add a new task here..."
+    );
+    expect(inputComponent).toBeInTheDocument();
+  });
+
+  it("Should change input value", () => {
+    render(<AddInput todos={[]} setTodos={mockedSetTodos} />);
+    const inputComponent = screen.getByPlaceholderText(
+      "Add a new task here..."
+    );
+    fireEvent.change(inputComponent, { target: { value: "jib dabbousa gaz" } });
+    expect(inputComponent.value).toBe("jib dabbousa gaz");
+  });
+
+  it("Should have an empty input when add btn is clicked", () => {
+    render(<AddInput todos={[]} setTodos={mockedSetTodos} />);
+    const inputComponent = screen.getByPlaceholderText(
+      "Add a new task here..."
+    );
+    const addBtn = screen.getByText("Add");
+
+    fireEvent.change(inputComponent, { target: { value: "jib dabbousa gaz" } });
+    fireEvent.click(addBtn);
+
+    expect(inputComponent.value).toBe("");
+  });
+});
